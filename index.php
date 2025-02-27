4be5a65475ab8e7827bc4de3f61ed2d23bc5902d
aeoyna <160382914+aeoyna@users.noreply.github.com>
2025-02-27 16:15:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -76,13 +76,14 @@ try {
             exit; // リダイレクト後はスクリプトの実行を停止
         }
     }
-    echo "<h1>星霜拾遺</h1>";
     <style>
         h1 {
             font-family: "ＭＳ Ｐ明朝", serif;
             color: blue;
         }
     </style>
+    
+    echo "<h1>星霜拾遺</h1>";
     // 並び順変更のリンク
     $newOrder = $order === 'ASC' ? 'desc' : 'asc';
     $orderLabel = $order === 'ASC' ? '小さい順' : '大きい順';
